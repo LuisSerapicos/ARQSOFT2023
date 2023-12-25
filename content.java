f336ee92f7902d9e06a3600662f7270910bf294f
LuisGitBucket <1230196@isep.ipp.pt>
2023-12-25 00:36:55 +0000

    Review microservice works has a microservice through api-gateway, but with hard coded relationships.

--- content.java
+++ content.java
@@ -5,6 +5,6 @@ public class Cmyhwh {
         System.out.println("mdwde");
         System.out.println("gxdis");
         System.out.println("xyyjo");
-        System.out.println("iojrm");
+        System.out.println("uywoc");
     }
 }
