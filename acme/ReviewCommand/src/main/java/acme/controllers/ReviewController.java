4c4077a8fc6a6b5939799f5b2c177f5247512bca
LuisGitBucket <luiserapicos18@gmail.com>
2023-12-26 16:13:07 +0000

    Modified docker-compose.yml to run two neo4j instances, because one instance only let's us run a single database. Implemented CQRS pattern to separate the reviews writes and reads concerns.

---
+++ acme/ReviewCommand/src/main/java/acme/controllers/ReviewController.java
@@ -0,0 +1,64 @@
+package acme.controllers;
+
+import acme.model.CreateReviewDTO;
+import acme.model.ReviewDTO;
+import acme.services.ReviewService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@Tag(name = "ReviewCommand", description = "Endpoints for managing Review")
+@RequestMapping("api/v1/reviews")
+@RestController
+class ReviewController {
+
+    @Autowired
+    private ReviewService rService;
+
+    @Operation(summary = "creates review")
+    @PostMapping("/products/{sku}/reviews")
+    public ResponseEntity<ReviewDTO> createReview(@PathVariable(value = "sku") final String sku, @RequestBody CreateReviewDTO createReviewDTO) {
+
+        final var review = rService.create(createReviewDTO, sku);
+
+        if(review == null){
+            return ResponseEntity.badRequest().build();
+        }
+
+        return new ResponseEntity<ReviewDTO>(review, HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "deletes review")
+    @DeleteMapping("/{reviewID}")
+    public ResponseEntity<Boolean> deleteReview(@PathVariable(value = "reviewID") final Long reviewID) {
+
+        Boolean rev = rService.DeleteReview(reviewID);
+
+        if (rev == null) return ResponseEntity.notFound().build();
+
+        if (rev == false) return ResponseEntity.unprocessableEntity().build();
+
+        return ResponseEntity.ok().body(rev);
+    }
+
+    @Operation(summary = "Accept or reject review")
+    @PutMapping("/acceptreject/{reviewID}")
+    public ResponseEntity<ReviewDTO> putAcceptRejectReview(@PathVariable(value = "reviewID") final Long reviewID, @RequestBody String approved){
+
+        try {
+            ReviewDTO rev = rService.moderateReview(reviewID, approved);
+
+            return ResponseEntity.ok().body(rev);
+        }
+        catch( IllegalArgumentException e ) {
+            return ResponseEntity.badRequest().build();
+        }
+        catch( ResourceNotFoundException e ) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ acme/ReviewCommand/src/main/java/acme/services/ReviewServiceImpl.java
@@ -0,0 +1,196 @@
+package acme.services;
+
+import acme.model.*;
+import acme.repositories.databases.ReviewDataBase;
+import acme.controllers.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+    private final ReviewDataBase reviewDataBase;
+    //private final ProductDataBase productDataBase;
+    //private final UserDataBase userDataBase;
+    //private final ReviewGenerator reviewGenerator;
+
+    //@Autowired
+    //UserService userService;
+
+    //@Autowired
+    //RatingService ratingService;
+
+    @Autowired
+    RestService restService;
+
+    @Override
+    public Iterable<Review> getAll() {
+        return reviewDataBase.getAllReview();
+    }
+
+    @Autowired
+    public ReviewServiceImpl(@Value("${review.interface.default}") String beanName, ApplicationContext context) {
+        this.reviewDataBase = context.getBean(beanName, ReviewDataBase.class);
+        //this.productDataBase = context.getBean(beanName2, ProductDataBase.class);
+        //this.userDataBase = context.getBean(beanName3, UserDataBase.class);
+        //this.reviewGenerator = context.getBean(reviewBeanName, ReviewGenerator.class);
+    }
+
+    @Override
+    public ReviewDTO create(final CreateReviewDTO createReviewDTO, String sku) {
+        final Optional<Product> product = Optional.of(new Product(100L, "skumegafixe2", "designation2", "description2"));
+
+        if (product.isEmpty()) return null;
+
+        final var user = Optional.of(new User("user1", "user1"));
+
+        if (user.isEmpty()) return null;
+
+        Rating rating = null;
+        Optional<Rating> r = Optional.of(new Rating(4.0));
+        if (r.isPresent()) {
+            rating = r.get();
+        }
+
+        LocalDate date = LocalDate.now();
+
+        String funfact = restService.getFunFact(date);
+
+        if (funfact == null) return null;
+
+        Review review = new Review(createReviewDTO.getUserID(), createReviewDTO.getReviewText(), date, product.get(), funfact, rating, user.get());
+
+        review = reviewDataBase.create(review);
+
+        if (review == null) return null;
+
+        return ReviewMapper.toDto(review);
+    }
+
+    @Override
+    public List<ReviewDTO> getReviewsOfProduct(String sku, String status) {
+
+        Optional<Product> product = Optional.of(new Product("skumegafixe1"));
+        if (product.isEmpty()) return null;
+
+        Optional<List<Review>> r = reviewDataBase.findByProductIdStatus(product.get(), status);
+
+        if (r.isEmpty()) return null;
+
+        return ReviewMapper.toDtoList(r.get());
+    }
+
+    @Override
+    public boolean addVoteToReview(Long reviewID, VoteReviewDTO voteReviewDTO) {
+
+        Optional<Review> review = this.reviewDataBase.findById(reviewID);
+
+        if (review.isEmpty()) return false;
+
+        Vote vote = new Vote(voteReviewDTO.getVote(), voteReviewDTO.getUserID());
+        if (voteReviewDTO.getVote().equalsIgnoreCase("upVote")) {
+            boolean added = review.get().addUpVote(vote);
+            if (added) {
+                Review reviewUpdated = this.reviewDataBase.updateVote(review.get(), "upVote");
+                return reviewUpdated != null;
+            }
+        } else if (voteReviewDTO.getVote().equalsIgnoreCase("downVote")) {
+            boolean added = review.get().addDownVote(vote);
+            if (added) {
+                Review reviewUpdated = this.reviewDataBase.updateVote(review.get(), "downVote");
+                return reviewUpdated != null;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Double getWeightedAverage(Product product) {
+
+        Optional<List<Review>> r = reviewDataBase.findByProductId(product);
+
+        if (r.isEmpty()) return 0.0;
+
+        double sum = 0;
+
+        for (Review rev : r.get()) {
+            Rating rate = rev.getRating();
+
+            if (rate != null) {
+                sum += rate.getRate();
+            }
+        }
+
+        return sum / r.get().size();
+    }
+
+    @Override
+    public Boolean DeleteReview(Long reviewId) {
+
+        Optional<Review> rev = reviewDataBase.findById(reviewId);
+
+        if (rev.isEmpty()) {
+            return null;
+        }
+        Review r = rev.get();
+
+        if (r.getUpVote().isEmpty() && r.getDownVote().isEmpty()) {
+            reviewDataBase.delete(r);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<ReviewDTO> findPendingReview() {
+
+        Optional<List<Review>> r = reviewDataBase.findPendingReviews();
+
+        if (r.isEmpty()) {
+            return null;
+        }
+
+        return ReviewMapper.toDtoList(r.get());
+    }
+
+    @Override
+    public ReviewDTO moderateReview(Long reviewID, String approved) throws ResourceNotFoundException, IllegalArgumentException {
+
+        Optional<Review> r = reviewDataBase.findById(reviewID);
+
+        if (r.isEmpty()) {
+            throw new ResourceNotFoundException("Review not found");
+        }
+
+        Boolean ap = r.get().setApprovalStatus(approved);
+
+        if (!ap) {
+            throw new IllegalArgumentException("Invalid status value");
+        }
+
+        Review review = reviewDataBase.updateApprovalStatus(r.get());
+
+        return ReviewMapper.toDto(review);
+    }
+
+
+    @Override
+    public List<ReviewDTO> findReviewsByUser(Long userID) {
+
+        final Optional<User> user = Optional.of(new User("user1", "user1"));
+
+        if (user.isEmpty()) return null;
+
+        Optional<List<Review>> r = reviewDataBase.findByUserId(user.get());
+
+        if (r.isEmpty()) return null;
+
+        return ReviewMapper.toDtoList(r.get());
+    }
+}
