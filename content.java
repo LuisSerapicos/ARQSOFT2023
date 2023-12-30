ee397eb2df06638411897354bc1945e7db6458ff
LuisGitBucket <1230196@isep.ipp.pt>
2023-12-30 18:58:48 +0000

    add code in files types: .xml,.java,.properties remove code in files types: .java,.xml

--- content.java
+++ content.java
@@ -4,6 +4,6 @@ public class Cmyhwh {
         System.out.println("fffmy");
         System.out.println("mdwde");
         System.out.println("gxdis");
-        System.out.println("ucugb");
+        System.out.println("ugial");
     }
 }
