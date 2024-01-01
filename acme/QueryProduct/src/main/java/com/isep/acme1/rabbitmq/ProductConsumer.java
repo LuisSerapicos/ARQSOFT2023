45c8d16dfc008226f17eadbfb6a35fd359f69fd5
LuisGitBucket <luiserapicos18@gmail.com>
2024-01-01 19:38:49 +0000

    Rebase with Microservics branch.

--- acme/QueryProduct/src/main/java/com/isep/acme1/rabbitmq/ProductConsumer.java
+++ acme/QueryProduct/src/main/java/com/isep/acme1/rabbitmq/ProductConsumer.java
@@ -10,10 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
 @Component
 @AllArgsConstructor
